7be9f56a12776454767f9636185274d808ec83d1
Artem Sharovatov <shrvtv@gmail.com>
2020-08-09 17:31:23 +0300

    refactoring brain-calc

--- brain_games/modules/brain_calc.py
+++ brain_games/modules/brain_calc.py
@@ -4,8 +4,9 @@ import brain_games.cli
 
 
 def get_list():
-    operations = ['addition', 'subtraction', 'multiplication']
+    operations = ['+', '-', '*']
     string = 'What is the result of the expression?'
+    right_answer = ''
     result = []
     tries = 3
     result.append(string)
@@ -15,23 +16,18 @@ def get_list():
         second_number = random.randint(1, 25)
         operation = random.choice(operations)
 
-        if operation == 'addition':
+        if operation == '+':
             right_answer = str(operator.add(first_number, second_number))
-            question = '{}: {} + {}'.format('Question', str(first_number), str(second_number))
-            result.append((question, right_answer))
-            tries -= 1
 
-        elif operation == 'subtraction':
+        elif operation == '-':
             right_answer = str(operator.sub(first_number, second_number))
-            question = '{}: {} - {}'.format('Question', str(first_number), str(second_number))
-            result.append((question, right_answer))
-            tries -= 1
 
-        elif operation == 'multiplication':
+        elif operation == '*':
             right_answer = str(operator.mul(first_number, second_number))
-            question = '{}: {} * {}'.format('Question', str(first_number), str(second_number))
-            result.append((question, right_answer))
-            tries -= 1
+
+        question = '{}: {} {} {}'.format('Question', str(first_number), operation, str(second_number))
+        result.append((question, right_answer))
+        tries -= 1
     return result
 
 
